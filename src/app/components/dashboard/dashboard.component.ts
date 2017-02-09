101725661c03bdbc1ed01c0f85d19fa7fbb79f9d
Lapchuk Oleksandr <oleksandr.lapchuk@neos.co.uk>
2017-02-09 17:29:01 +0200

    finish dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import {Component} from "@angular/core";
 import {HttpClient} from "../../shared/services/http.client";
+import {AccountService} from "../option/account/services/account.service";
 
 @Component({
   templateUrl: "./dashboard.html",
@@ -7,11 +8,19 @@ import {HttpClient} from "../../shared/services/http.client";
 })
 
 export class DashboardComponent {
-  constructor(private http: HttpClient) {
+  accounts = [];
+
+  constructor(private http: HttpClient, public Account: AccountService) {
     this.http = http;
 
+
+    Account.getList()
+      .subscribe((resp) => {
+        this.accounts = resp.data;
+        console.log(resp);
+      });
     /*this.http.get('http://localhost:8742/user/profile')
-      .subscribe( resp => console.log(resp));*/
+     .subscribe( resp => console.log(resp));*/
 
   }
 }
