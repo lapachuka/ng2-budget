faf644b743ec4cb352bccd28c9adf82f9fd19f96
Lapchuk Oleksandr <oleksandr.lapchuk@neos.co.uk>
2017-05-04 17:09:28 +0300

    transaction

--- src/app/components/transaction/details/transaction.details.component.ts
+++ src/app/components/transaction/details/transaction.details.component.ts
@@ -1,5 +1,11 @@
 import {Component} from "@angular/core";
 import {MdDialogRef} from "@angular/material";
+import {FormControl} from "@angular/forms";
+import {AccountService} from "../../../services/account.service";
+import {CategoryService} from "../../../services/category.service";
+import {TransactionService} from "../../../services/transaction.service";
+import {IMyOptions, IMyDateModel} from "mydatepicker";
+import {Transaction} from "../transaction.model";
 
 @Component({
   selector: 'transaction-details',
@@ -8,14 +14,101 @@ import {MdDialogRef} from "@angular/material";
 })
 
 
-export class TransactionDetailsComponent{
-  name: string = '';
+export class TransactionDetailsComponent {
 
-  constructor(public dialogRef: MdDialogRef<TransactionDetailsComponent>){
+  mode;
+  type;
+  transaction: Transaction;
+  categories = [];
+  filteredCateogires = [];
+  accounts = [];
+  transactionId;
+
+  onDateChanged(event: IMyDateModel) {
+  }
+
+  constructor(public dialogRef: MdDialogRef<TransactionDetailsComponent>,
+              private categoryService: CategoryService,
+              private transactionService: TransactionService,
+              public Account: AccountService) {
+
+  }
+
+  filterCategory(val: string) {
+    this.filteredCateogires = val ? this.categories.filter((s) => new RegExp(val, 'gi').test(s.name)) : this.categories;
+  }
+
+  ngOnInit() {
+    this.transactionId && this.fillCurrentTransaction();
+    this.fillCategory(this.type || this.transaction.type);
+    this.fillAccounts()
+  }
+
+  fillCurrentTransaction() {
+    this.transactionService.getById(this.transactionId)
+      .subscribe((curTran) => {
+        curTran.type = this.type;
+        this.transaction = new Transaction(curTran);
+      });
+  }
+
+
+  //todo: need refactor this part
+  fillCategory(type) {
+    this.categoryService.getList(type)
+      .subscribe((resp) => {
+        let categoriesArray = [];
+
+        resp.data.forEach((category) => {
+          if (category.sub_categories) {
+            categoriesArray = categoriesArray.concat(category.sub_categories);
+          }
+        });
+
+        this.categories = resp.data;
+        this.categories = this.categories.concat(categoriesArray);
+        if (!this.transactionId) {
+          this.transaction.category_id = this.categories[0].id;
+        }
+      });
+  }
+
+  fillAccounts() {
+    this.Account.getList()
+      .subscribe((resp) => {
+          this.accounts = resp.data;
+          if (!this.transactionId) {
+            this.transaction.account_id = this.accounts[0].id;
+          }
+        }
+      )
   }
 
   ok() {
-    this.dialogRef.close(true);
+    let cDate = this.transaction.curDate.date;
+    this.transaction.date = new Date(cDate.year, cDate.month - 1, cDate.day);
+    this.transactionService.create(this.transaction)
+      .subscribe(() => {
+        this.dialogRef.close(this.transaction);
+      });
   }
+
+  update() {
+    let cDate = this.transaction.curDate.date;
+    this.transaction.date = new Date(cDate.year, cDate.month - 1, cDate.day);
+    this.transactionService.update(this.transaction)
+      .subscribe(() => {
+        this.dialogRef.close(this.transaction);
+      });
+  }
+
+  cancel() {
+    this.dialogRef.close(false);
+  }
+
+  addMore() {
+
+  }
+
 }
 
--- src/app/components/transaction/details/transaction.details.module.ts
+++ src/app/components/transaction/details/transaction.details.module.ts
@@ -1,10 +1,24 @@
 import {NgModule} from "@angular/core";
-import {MdButtonModule} from "@angular/material";
+import {MdButtonModule, MdSelectModule, MdInputModule, MdAutocompleteModule} from "@angular/material";
 import {TransactionDetailsComponent} from "./transaction.details.component";
+import {CommonModule} from "@angular/common";
+import {SelectModule} from "ng2-select";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {DatePickerModule} from "ng2-datepicker";
+import {MyDatePickerModule} from "mydatepicker";
 
 @NgModule({
   imports: [
-    MdButtonModule
+    MdButtonModule,
+    MdInputModule,
+    MdSelectModule,
+    CommonModule,
+    DatePickerModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MyDatePickerModule,
+    MdAutocompleteModule,
+    SelectModule
   ],
   declarations: [
     TransactionDetailsComponent
